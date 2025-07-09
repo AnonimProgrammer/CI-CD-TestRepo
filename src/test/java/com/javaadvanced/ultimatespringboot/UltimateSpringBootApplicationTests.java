14c853849dafcf731bb7e055a8bc2713f043a644
AnonimProgrammer <zephyr11ff@gmail.com>
2025-07-09 20:09:01 +0400

    project basic configs added

---
+++ src/test/java/com/javaadvanced/ultimatespringboot/UltimateSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaadvanced.ultimatespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UltimateSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
