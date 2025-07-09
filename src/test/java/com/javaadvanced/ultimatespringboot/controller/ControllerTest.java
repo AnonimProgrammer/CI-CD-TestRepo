fc8dea2549137658ad16bb7c96beea0251b2c4e5
AnonimProgrammer <zephyr11ff@gmail.com>
2025-07-09 20:20:06 +0400

    controller logic is changed, controller test is added.

---
+++ src/test/java/com/javaadvanced/ultimatespringboot/controller/ControllerTest.java
@@ -0,0 +1,23 @@
+package com.javaadvanced.ultimatespringboot.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(Controller.class)
+public class ControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void testSayHello() throws Exception {
+        mockMvc.perform(get("/api"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hello, World!"));
+    }
+}
