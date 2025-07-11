299f1fdd27c516289de43799ab65e6f5403ad324
AnonimProgrammer <zephyr11ff@gmail.com>
2025-07-11 14:15:08 +0400

    controller testing is updated.

--- src/test/java/com/javaadvanced/ultimatespringboot/controller/ControllerTest.java
+++ src/test/java/com/javaadvanced/ultimatespringboot/controller/ControllerTest.java
@@ -16,7 +16,7 @@ public class ControllerTest {
 
     @Test
     void testSayHello() throws Exception {
-        mockMvc.perform(get("/api/hello"))
+        mockMvc.perform(get("/api/helo"))
                 .andExpect(status().isOk())
                 .andExpect(content().string("Hello, World!"));
     }
