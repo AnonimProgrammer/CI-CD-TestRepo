e37594e1115d156d8123aef6f9040e1f266a09a9
AnonimProgrammer <zephyr11ff@gmail.com>
2025-07-12 17:36:36 +0400

    User Entity is designed. UserRepository and UserService are added.

---
+++ src/main/java/com/javaadvanced/ultimatespringboot/model/entity/User.java
@@ -0,0 +1,30 @@
+package com.javaadvanced.ultimatespringboot.model.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
