7b0571eddb83f81edc5b0608560645e1c300cc4c
AnonimProgrammer <zephyr11ff@gmail.com>
2025-07-30 14:04:30 +0400

    .

--- update-k8s.sh
+++ update-k8s.sh
@@ -6,10 +6,8 @@ BRANCH="master"
 DEPLOYMENT_NAME="test-app"
 SHA_FILE="/Users/omarismailov/Desktop/MyDocs/java_education/java_advanced/ultimate-spring-boot/.last_sha"
 
-# Get latest commit SHA from GitHub
 LATEST_SHA=$(curl -s https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/commits/${BRANCH} | jq -r '.sha')
 
-# Load last SHA (if any)
 LAST_SHA=$(cat "$SHA_FILE" 2>/dev/null)
 
 if [ "$LATEST_SHA" != "$LAST_SHA" ]; then
